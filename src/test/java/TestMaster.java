a26248d7541950dc2e35d2ec4f58a1b177027c67
Prashanna <prashbn@gmail.com>
2015-07-24 19:20:13 +1000

    Intial

--- src/test/java/TestMaster.java
+++ src/test/java/TestMaster.java
@@ -6,7 +6,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 features ={"src/test/resources/login.feature"},
-tags={"@Login","~@TC001"},
 plugin = {"html:target/cucumber-html-report", "pretty:target/cucumber-json-report.json"},
 strict = true
 )
