98736b4e9f970a90444338e267094155c3130e6f
Prashanna <prashbn@gmail.com>
2015-07-30 17:07:03 +1000

    Vansah

--- src/test/java/StepDefn.java
+++ src/test/java/StepDefn.java
@@ -55,7 +55,7 @@ public class StepDefn {
 		 String JENKINS_BUILD_URL = System.getProperty("jenkins.buildUrl");
 		 String VANSAH_AGENT="AGENT2";
 		 String VANSAH_TOKEN="FLEXATSI15";
-	    
+		 System.out.println(" JENKINS_BUILD_URL :" + JENKINS_BUILD_URL);
 		System.out.println(" VANSAH_PACKAGE :" + VANSAH_PACKAGE);
 		System.out.println(" VANSAH_RELEASE :" + VANSAH_RELEASE);
 		System.out.println(" VANSAH_BUILD :" + VANSAH_BUILD);
