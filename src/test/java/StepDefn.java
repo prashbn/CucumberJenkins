107b2f4f46faf9632939a5560d7355c42237022f
Prashanna <prashbn@gmail.com>
2015-07-31 19:34:05 +1000

    jenkins hook

--- src/test/java/StepDefn.java
+++ src/test/java/StepDefn.java
@@ -27,7 +27,7 @@ public class StepDefn {
     
     private void setup() {
     	
-    	System.out.println("Jenkins trigger");
+    	System.out.println("Jenkins trigger1");
     	
     	
 		// TODO Auto-generated method stub
